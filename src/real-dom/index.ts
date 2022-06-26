c0cf7814c08fab6f451953aa0dcff23edcda9f67
kritzona <hangookhelly@gmail.com>
2022-06-27 01:21:01 +0300

    feat: add element factory

--- src/real-dom/index.ts
+++ src/real-dom/index.ts
@@ -1,16 +1,33 @@
 import VirtualDomNode from '@/virtual-dom/virtual-dom-node';
 
 class RealDom {
+  private _elementMap: Map<symbol, HTMLElement> = new Map();
+
   constructor(private _rootElement: HTMLElement) {}
+
+  private _elementFactory(node: VirtualDomNode): HTMLElement {
+    const { key, tagName } = node;
+
+    let element;
+    if (this._elementMap.get(key)) {
+      element = this._elementMap.get(key);
+    } else {
+      element = document.createElement(tagName);
+
+      this._elementMap.set(key, element);
+    }
+
+    return element;
+  }
 
   public mountRoot(node: VirtualDomNode) {
     this.mount(node, this._rootElement);
   }
 
   public mount(node: VirtualDomNode, parentElement: HTMLElement) {
-    const { tagName, props, children, component } = node;
+    const { props, children, component } = node;
 
-    const element = document.createElement(tagName);
+    const element = this._elementFactory(node);
 
     for (const [prop, value] of Object.entries(props)) {
       element.setAttribute(prop, value);
