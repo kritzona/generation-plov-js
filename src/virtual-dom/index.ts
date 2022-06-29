a2432aa5d065bb03e2a1d9f39324be460a3be6fc
krtizona <hangookhelly@gmail.com>
2022-06-29 20:49:52 +0300

    refactor: rework component init

--- src/virtual-dom/index.ts
+++ src/virtual-dom/index.ts
@@ -1,5 +1,6 @@
+import VirtualDomComponentNode from '@/virtual-dom/nodes/virtual-dom-component-node';
 import RealDom from '@/real-dom';
-import { VirtualDomNode } from './types';
+import { VirtualDomNode, VirtualDomNodeChild } from './types';
 
 class VirtualDom {
   private _tree: isNullable<VirtualDomNode> = null;
@@ -14,7 +15,31 @@ class VirtualDom {
     return this._realDom;
   }
 
+  public init(node: VirtualDomNodeChild) {
+    if (typeof node === 'string') {
+      return;
+    }
+
+    const { children } = node;
+
+    if (node instanceof VirtualDomComponentNode) {
+      node.init();
+    }
+
+    children.forEach((childNode) => {
+      if (childNode instanceof VirtualDomComponentNode) {
+        childNode.init();
+      }
+    });
+
+    this._apply(node);
+  }
+
   public patch(node: VirtualDomNode) {
+    this._apply(node);
+  }
+
+  private _apply(node: VirtualDomNode) {
     this._tree = node;
 
     this._realDom.patchBaseElement(this._tree);
